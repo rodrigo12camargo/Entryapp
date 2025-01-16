32207e9527c5a7d6959a9e04b8a484311de5497e
OliverVlqz <oliveratom772@gmail.com>
2025-01-16 10:23:29 -0600

    Pantallas de usuario

---
+++ app/src/main/java/net/entrypay/app/user_profile.kt
@@ -0,0 +1,16 @@
+package net.entrypay.app
+
+import android.os.Bundle
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class user_profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_user_profile)
+
+
+    }
+}
---
+++ app/src/main/java/net/entrypay/app/user_profile_password.kt
@@ -0,0 +1,16 @@
+package net.entrypay.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class user_profile_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user_profile_password)
+
+    }
+}
